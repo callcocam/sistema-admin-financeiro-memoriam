0337646193c3b5854a719ffc1403849932636ec7
callcocam <callcocam@gmail.com>
2021-03-05 09:11:54 -0300

    input select

--- app/SIGA/Form/resources/views/fields/select-dropdown.blade.php
+++ app/SIGA/Form/resources/views/fields/select-dropdown.blade.php
@@ -4,31 +4,31 @@
         {{ _translate(sprintf('form.%s', $field->name),$field->label) }}
     </label>
     <div class="mt-1 relative">
-        <button @click="open=true" type="button" aria-haspopup="listbox" aria-expanded="true"
-                aria-labelledby="listbox-label"
-                class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        <input @click="open=true" type="text" wire:model="SingleselectSearch.{{ $field->name }}"
+               class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
 
-      <span class="flex items-center">
+        <span class="flex items-center absolute top-2 left-2">
            @isset($this->SingleselectSelected[$field->name])
-              @if (!is_string($this->SingleselectSelected[$field->name]))
-                  <img
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt="" class="flex-shrink-0 h-6 w-6 rounded-full">
-              @endif
-              <span class="ml-3 block truncate">
+                @if (!is_string($this->SingleselectSelected[$field->name]))
+                    <img
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt="" class="flex-shrink-0 h-6 w-6 rounded-full">
+                @endif
+                <span class="ml-3 block truncate" x-show="!open">
              {{ $this->SingleselectSelected[$field->name] }}
             </span>
-          @else
-              <span class="ml-3 block truncate">
+            @else
+                <span class="ml-3 block truncate">
                 @lang('strings.select-item')
             </span>
-          @endisset
+            @endisset
       </span>
-            <span class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+        <span class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
         <!-- Heroicon name: solid/selector -->
       <x-c-icon icon="swap-vertical" class="h-5 w-5"/>
       </span>
-        </button>
+        </input>
+
         <div class="absolute mt-1 w-full rounded-md bg-white shadow-lg" x-show="open">
             <ul tabindex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-item-3"
                 class="max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
