825283d034495fb762fe70c27866a00f92fb663b
callcocam <callcocam@gmail.com>
2021-03-29 09:22:10 -0300

    add document clients

--- app/Http/Livewire/Incomes/ListComponent.php
+++ app/Http/Livewire/Incomes/ListComponent.php
@@ -65,4 +65,12 @@ class ListComponent extends TableComponent
     {
         return "incomes";
     }
+
+
+    public function createLink()
+    {
+        return view(table_includes($this->create_view_button()))->with('route', 'admin.load-clients.api');
+
+    }
+
 }
